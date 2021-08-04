72e40b6ccab16f8640a33321bb8c278ef6f96626
Julio-C-M <mcjjuliocesarr@gmail.com>
2021-08-04 15:42:52 -0500

    Agregando register page y forgot password

--- src/app/login/register/register.page.ts
+++ src/app/login/register/register.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  styleUrls: ['../login.page.scss'], /* Se usan los mismos Estilos que el login*/
 })
 export class RegisterPage implements OnInit {
 
